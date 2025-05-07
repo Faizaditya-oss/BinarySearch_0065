99df7bc23f8192fdbf955f1ac32bc45b0a9221f4
Faizaditya-oss <faiz.aditya.ft24@mail.umy.ac.id>
2025-05-07 16:04:11 +0700

    mencari elemen

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -64,3 +64,15 @@ void display ()
     }
     cout << endl;
 }
+
+void binarySearch()
+{
+    char ulang;
+    do 
+    {
+        cout << "\n===================================\n";
+        cout << "     Pencarian Binary Search     \n";
+        cout << "===================================\n";
+        cout << "Masukkan elemen yang ingin di cari: ";
+    }
+}
